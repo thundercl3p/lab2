2d2edddf555a8bff18728983e4d46d293e56027e
thundercl3p <kiselyov_artyom@rambler.ru>
2025-05-14 23:38:03 +0300

    Apply Mozilla code style with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,22 @@
-// Простая программа, которая приветствует пользователя
+// 
 #include <iostream>
 #include <string>
 
+<<<<<<< HEAD
 int main() {
     std::string name;
-    std::cout << "Enter your name: ";  // Запрос ввода пользователя
-    std::cin >> name;                  // Чтение ввода пользователя
-    std::cout << "Hello world from " << name << std::endl;  // Вывод приветствия
+    std::cout << "Enter your name: ";  //
+    std::cin >> name;                  //
+    std::cout << "Hello world from " << name << std::endl;  //
     return 0;
+=======
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: "; // Prompt for user input
+  std::cin >> name;                 // Read user input
+  std::cout << "Hello world from " << name << std::endl; // Output greeting
+  return 0;
+>>>>>>> 667e1c2 (Apply Mozilla code style with clang-format)
 }
