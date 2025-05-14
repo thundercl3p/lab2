b473c264550abe00889ebed690dddd1851517e1d
thundercl3p <kiselyov_artyom@rambler.ru>
2025-05-14 23:30:49 +0300

    Add code comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
+// Simple program that greets the user
 #include <iostream>
 #include <string>
 
 int main() {
     std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
+    std::cout << "Enter your name: ";  // Prompt for user input
+    std::cin >> name;                  // Read user input
+    std::cout << "Hello world from " << name << std::endl;  // Output greeting
     return 0;
 }
